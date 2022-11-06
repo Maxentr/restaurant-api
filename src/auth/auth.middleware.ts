2270c2ba67a2413a2fd51d2fd4e4e508a02b9d21
maxentr <rouaultmaxent@gmail.com>
2022-11-06 15:21:28 +0100

    docs(Auth middleware): function documentation refactor: fix status when invalid token

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -3,7 +3,23 @@ import { Role } from "../users/users.schema"
 import { AuthService } from "./auth.service"
 
 type RoleString = keyof typeof Role
-const AuthMiddleware = (...roleRestriction: RoleString[]) => {
+
+/**
+ * @function
+ * 
+ * Check if the user is authenticated and has the right role
+ * 
+ * @param {RoleString[]} roleRestriction - Destructured array of roles that are allowed to access the route
+ * 
+ * @example
+ * This route is only accessible by an admin:
+ * Auth("ADMIN")
+ * 
+ * This route is accessible by an admin or a customer:
+ * Auth("ADMIN", "CUSTOMER")
+ *
+ */
+const Auth = (...roleRestriction: RoleString[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization
 
@@ -20,9 +36,9 @@ const AuthMiddleware = (...roleRestriction: RoleString[]) => {
         res.status(403).send({ message: "Forbidden" })
       }
     } catch (error) {
-      res.status(403).send({ message: "Invalid token" })
+      res.status(400).send({ message: "Invalid token" })
     }
   }
 }
 
-export default AuthMiddleware
+export default Auth
