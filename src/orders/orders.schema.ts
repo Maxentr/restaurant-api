6344f0bfb21dfe44b2c5a8b876866bfd69810f57
maxentr <rouaultmaxent@gmail.com>
2022-11-22 12:01:45 +0100

    refactor(orders): change schema for better typing

--- src/orders/orders.schema.ts
+++ src/orders/orders.schema.ts
@@ -1,4 +1,7 @@
 import { Schema, model, Types } from "mongoose"
+import { Dish } from "../dishes/dishes.schema"
+import { Drink } from "../drinks/drinks.schema"
+import { Menu } from "../menus/menus.schema"
 import { User } from "../users/users.schema"
 
 export enum OrderItemType {
@@ -7,36 +10,72 @@ export enum OrderItemType {
   DISH = "DISH",
 }
 
-export type OrderItem = {
+type OrderItemDefault = {
   _id: Types.ObjectId
-  item: Types.ObjectId
   type: OrderItemType
   quantity: number
   totalPrice: number
-  elementsId?: Types.ObjectId[]
 }
+
+export type OrderItemMenu = OrderItemDefault & {
+  menu: Types.ObjectId
+  type: OrderItemType.MENU
+  choicesId: Types.ObjectId[]
+}
+
+export type OrderItemDish = OrderItemDefault & {
+  dish: Types.ObjectId
+  type: OrderItemType.DISH
+}
+
+export type OrderItemDrink = OrderItemDefault & {
+  drink: Types.ObjectId
+  type: OrderItemType.DRINK
+  sizeId: Types.ObjectId
+}
+
+export type OrderItem = OrderItemMenu | OrderItemDish | OrderItemDrink
 
 export type Order = {
   _id: Types.ObjectId
   customer: Types.ObjectId
-  order: OrderItem[]
+  items: OrderItem[]
   total: number
   createdAt: Date
 }
 
 // Mongooose schema
 // This is an equivalent of Typescript types above
-const orderItemSchema = new Schema<OrderItem>({
-  item: { type: Schema.Types.ObjectId, required: true },
+
+const OrderItemDefaultSchema = {
   type: { type: String, enum: OrderItemType, required: true },
   quantity: { type: Number, required: true },
   totalPrice: { type: Number, required: true },
-  elementsId: { type: [Schema.Types.ObjectId], required: false },
+}
+
+const orderItemMenuSchema = new Schema<OrderItemMenu>({
+  ...OrderItemDefaultSchema,
+  menu: { type: Schema.Types.ObjectId, ref: Menu, required: true },
+  choicesId: { type: [Schema.Types.ObjectId], required: true },
+})
+
+const orderItemDishSchema = new Schema<OrderItemDish>({
+  ...OrderItemDefaultSchema,
+  dish: { type: Schema.Types.ObjectId, ref: Dish, required: true },
+})
+
+const orderItemDrinkSchema = new Schema<OrderItemDrink>({
+  ...OrderItemDefaultSchema,
+  drink: { type: Schema.Types.ObjectId, ref: Drink, required: true },
+  sizeId: { type: Schema.Types.ObjectId, required: true },
 })
 
 const ordersSchema = new Schema<Order>({
   customer: { type: Schema.Types.ObjectId, ref: User, required: true },
-  order: { type: [orderItemSchema], required: true },
+  items: {
+    type: [orderItemMenuSchema || orderItemDishSchema || orderItemDrinkSchema],
+    required: true,
+  },
   total: { type: Number, required: true },
   createdAt: { type: Date, required: true },
 })
